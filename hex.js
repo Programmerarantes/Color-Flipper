13cdd5e5ea42ce0ca4fa2cfcddcb0a7e467f9b8a
Matheus Arantes <125316650+Programmerarantes@users.noreply.github.com>
2023-05-18 21:34:41 -0300

    third commit

--- hex.js
+++ hex.js
@@ -13,5 +13,5 @@ btn.addEventListener("click", function() {
 });
 
 function getRandomNumber() {
-    return Math.floor(Math.random() + hex.length);
+    return Math.floor(Math.random() * hex.length);
 }
