fd510a11ddd30e8180f738ea4c203f9c0883bd5b
Matheus Arantes <125316650+Programmerarantes@users.noreply.github.com>
2023-05-18 21:22:50 -0300

    second commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const colors = ["red", "blue", "green", "black"];
+
+const btn = document.querySelector("#btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function() {
+//get random number between 0 - 3    
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
